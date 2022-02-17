be19333bc19d57729aaa80f741c4c78af743aba6
Kazakoff <kazakoff@1k129s15.dune.edu>
2022-02-17 14:30:32 +0200

    remove todo

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -11,9 +11,7 @@ package javaapplication4;
  */
 public class JavaApplication4 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
 
     }
